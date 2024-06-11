27765ff59369700acf9bfc1ac61267a7ad4aa49d
Kyle Lacy <kylewlacy@users.noreply.github.com>
2024-06-10 23:25:46 -0700

    Add Go package (binary-only) (#23)
    * Add initial Go toolchain
    
    * Tweak Go package structure and set `$GOROOT` when used as a dep
    
    * Add `go.goInstall()` function
    
    * Fix `go.goInstall()` when there is no `go.sum` file
    
    * Add doc comments to `go` package
    
    * Add Go CLI example
    
    * Clean up Go example project

---
+++ examples/go_cli/go.mod
@@ -0,0 +1,11 @@
+module github.com/brioche-dev/brioche-packages/examples/go_cli
+
+go 1.22.4
+
+require (
+	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/schollz/progressbar/v3 v3.14.4 // indirect
+	golang.org/x/sys v0.21.0 // indirect
+	golang.org/x/term v0.21.0 // indirect
+)
---
+++ examples/go_cli/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"flag"
+	"os"
+	"time"
+
+	"github.com/schollz/progressbar/v3"
+)
+
+func main() {
+	wait := flag.Duration("wait", time.Second, "duration to wait")
+	flag.Parse()
+
+	if wait == nil {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	bar := progressbar.NewOptions64(wait.Milliseconds())
+	start := time.Now()
+
+	for {
+		elapsed := time.Since(start)
+		if elapsed > *wait {
+			break
+		}
+
+		bar.Set64(elapsed.Milliseconds())
+		time.Sleep(time.Millisecond * 100)
+	}
+
+	bar.Finish()
+	println()
+}
